4b5684f2fae9370aeb64bfb29e4055b097f3ac9a
donetd01 <donetd01@kits.kfplc.com>
2019-12-19 09:59:22 +0200

    learn

--- src/test/java/tests/StringReverse.java
+++ src/test/java/tests/StringReverse.java
@@ -3,12 +3,18 @@ import java.lang.*;
 
 public class StringReverse {
 
-    private String reversedString;
+    int i;
 
     public void printStringReverseWay1(String reversedString) {
         char[] array = reversedString.toCharArray();
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i]);
+        }
+    }
+
+    public void returnArray(int[] createArray) {
+        for (i = 0; i < createArray.length; i++) {
+            System.out.println(createArray[i]);
         }
     }
 }
--- src/test/java/tests/StringReverseTest.java
+++ src/test/java/tests/StringReverseTest.java
@@ -11,4 +11,9 @@ public class StringReverseTest extends StringReverse{
         str.printStringReverseWay1("love");
     }
 
+    @Test
+    public void playWithArray() {
+        str.returnArray(new int[]{1,3,4,5,5,5,8});
+    }
+
 }
