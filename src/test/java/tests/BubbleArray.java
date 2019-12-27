a0f1f9eafdeb219ee3c7ad5e107656c99dd8bdeb
donetd01 <donetd01@kits.kfplc.com>
2019-12-27 09:37:34 +0200

    learn

--- src/test/java/tests/BubbleArray.java
+++ src/test/java/tests/BubbleArray.java
@@ -1,6 +1,25 @@
 package tests;
 
+import java.util.Arrays;
+
 public class BubbleArray {
 
+    boolean isSorted = false;
+    int buf;
+
+    public BubbleArray(int[] mas) {
+        while (!isSorted) {
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
 
 }
---
+++ src/test/java/tests/BubbleArrayTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class BubbleArrayTest {
+
+    BubbleArray first = new BubbleArray(new int[]{11, 344, 54, 5, 6});
+
+    @Test
+    public void tryBubble(){
+        System.out.println(first);
+    }
+}
